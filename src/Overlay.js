7d83b4cca4bcc281d5be67bb3d15ba02d059883e
Sebastian Friedel <sebastian@ffwd.consulting>
2020-07-01 12:51:47 +0200

    refactor: refactor Overlay.js to avoid monkey patching original class

--- src/Overlay.js
+++ src/Overlay.js
@@ -1,29 +1,32 @@
 import { get } from 'svelte/store'
-import Overlay, { getTranslation } from './internal/Overlay.svelte'
+import InternalOverlay, { getTranslation } from './internal/Overlay.svelte'
 import { translateElement } from './internal/utils'
-
 import { stores } from './FluentProvider.svelte'
 
-if (!process.browser) {
-  if (Overlay.$$render) {
-    const { JSDOM } = require('jsdom')
+let Overlay = InternalOverlay
 
-    const $$render = Overlay.$$render
-    Overlay.$$render = (result, props, bindings, slots) => {
-      const html = $$render(result, props, bindings, slots)
-      const { getBundle } = stores()
-      const { id, args } = props
-      const $getBundle = get(getBundle)
-      const bundle = id ? $getBundle(id) : null
-      const msg = bundle ? bundle.getMessage(id) : null
-      const translation = getTranslation(bundle, msg, args)
-      const { document } = new JSDOM('<!DOCTYPE html>').window
-      const body = document.createElement('body')
-      body.innerHTML = html
-      const rootNode = body.firstChild
-      translateElement(rootNode, translation)
-      return body.innerHTML
-    }
+if (!process.browser && InternalOverlay.$$render) {
+  const { JSDOM } = require('jsdom')
+
+  Overlay = function Overlay (...args) {
+    return new InternalOverlay(...args)
+  }
+
+  Overlay.prototype = InternalOverlay
+  Overlay.$$render = (result, props, bindings, slots) => {
+    const html = InternalOverlay.$$render(result, props, bindings, slots)
+    const { getBundle } = stores()
+    const { id, args } = props
+    const $getBundle = get(getBundle)
+    const bundle = id ? $getBundle(id) : null
+    const msg = bundle ? bundle.getMessage(id) : null
+    const translation = getTranslation(bundle, msg, args)
+    const { document } = new JSDOM('<!DOCTYPE html>').window
+    const body = document.createElement('body')
+    body.innerHTML = html
+    const rootNode = body.firstChild
+    translateElement(rootNode, translation)
+    return body.innerHTML
   }
 }
 
