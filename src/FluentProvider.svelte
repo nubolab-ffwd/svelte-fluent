12ad13de5141f39a9f3aa1a89555a44f61628fc7
Sebastian Friedel <sebastian@ffwd.consulting>
2020-12-18 10:56:21 +0100

    feat: expose stores for retrieving translations directly from FluentProvider
    This makes it possible to use translated values directly in component JS code.

--- src/FluentProvider.svelte
+++ src/FluentProvider.svelte
@@ -1,7 +1,7 @@
 <script context="module">
-  import { getContext, setContext } from 'svelte'
+  import { getContext, setContext, createEventDispatcher } from 'svelte'
   import { mapBundleSync } from '@fluent/sequence'
-  import { derived, readable, writable } from 'svelte/store'
+  import { derived, writable } from 'svelte/store'
   import { CachedSyncIterable } from 'cached-iterable'
 
   const CONTEXT_KEY = {}
@@ -9,15 +9,32 @@
   export function stores () {
     const ctx = getContext(CONTEXT_KEY)
     if (!ctx) {
-      console.error('<FluentProvider/> was not found in component hierarchy.')
-      return {
-        getBundle: readable(id => null)
+      console.error('[svelte-fluent] <FluentProvider/> was not found in component hierarchy.')
+      return
+    }
+    const { bundles, dispatch } = ctx
+    const getBundle = derived(bundles, $bundles => id => mapBundleSync($bundles, id))
+    const getTranslation = derived(bundles, $bundles => (id, args) => {
+      const bundle = mapBundleSync($bundles, id)
+      if (bundle === null) {
+        dispatch('error', `[svelte-fluent] Translation missing: "${id}"`)
+        return { value: id, attributes: {} }
       }
-    }
-    const { bundles } = ctx
-    return {
-      getBundle: derived(bundles, $bundles => id => mapBundleSync($bundles, id))
-    }
+      const msg = bundle.getMessage(id)
+      if (msg === null) {
+        dispatch('error', `[svelte-fluent] Translation missing: "${id}"`)
+        return { value: id, attributes: {} }
+      }
+      const value = bundle.formatPattern(msg.value, args)
+      const attributes = Object.fromEntries(Object.entries(msg.attributes || {}).map(
+        ([name, pattern]) => ([name, bundle.formatPattern(pattern, args)]))
+      )
+      return { value, attributes }
+    })
+    const translate = derived(getTranslation, $getTranslation => (id, args) => {
+      return $getTranslation(id, args).text
+    })
+    return { getBundle, getTranslation, translate }
   }
 </script>
 
@@ -28,7 +45,8 @@
   $: set(CachedSyncIterable.from(bundles))
 
   setContext(CONTEXT_KEY, {
-    bundles: { subscribe }
+    bundles: { subscribe },
+    dispatch: createEventDispatcher()
   })
 </script>
 
