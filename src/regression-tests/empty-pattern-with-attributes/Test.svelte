79716538cc7082f355dcb28e44a5812b1a6dc4d5
Sebastian Friedel <sebastian@ffwd.consulting>
2023-02-22 16:55:26 +0100

    fix: handle translations with empty pattern that define attributes
    fixes #695

---
+++ src/regression-tests/empty-pattern-with-attributes/Test.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { FluentBundle, FluentResource } from '@fluent/bundle';
+	import { FluentProvider, Localized } from '@nubolab-ffwd/svelte-fluent';
+
+	const translations = `
+empty =
+	.text = Some text
+`;
+	const bundle = new FluentBundle('en');
+	bundle.addResource(new FluentResource(translations));
+</script>
+
+<FluentProvider bundles={[bundle]}>
+	<Localized id="empty" let:attrs>
+		{attrs.text}
+	</Localized>
+</FluentProvider>
