7e23c52443fc07f49810f4d947818760f7216126
Sebastian Friedel <sebastian@ffwd.consulting>
2023-03-06 13:52:47 +0100

    test: add for localize store output

---
+++ src/e2e/localize-store/Content.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { localize } from '@nubolab-ffwd/svelte-fluent';
+</script>
+
+<div id="plain">
+	{$localize('plain')}
+	{$localize('plain.attribute')}
+</div>
+<div id="with-vars">
+	{$localize('with-vars', { name: 'with variables' })}
+	{$localize('with-vars.attribute', { name: 'with variables' })}
+</div>
+7
---
+++ src/e2e/localize-store/Test.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { FluentBundle, FluentResource } from '@fluent/bundle';
+	import { FluentProvider } from '@nubolab-ffwd/svelte-fluent';
+	import Content from './Content.svelte';
+
+	const translations = `
+plain = Test
+	.attribute = Attribute
+with-vars = Test {$name}
+	.attribute = Attribute {$name}
+`;
+	const bundle = new FluentBundle('en');
+	bundle.addResource(new FluentResource(translations));
+</script>
+
+<FluentProvider bundles={[bundle]}>
+	<Content />
+</FluentProvider>
