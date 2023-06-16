f3259d0f5ce9bbc17fae4bc8c5a4678016cf2d8b
Sebastian Friedel <sebastian@ffwd.consulting>
2023-06-16 15:53:23 +0200

    fix: prevent exception in svelte-devtools

--- src/lib/internal/FluentProvider.svelte
+++ src/lib/internal/FluentProvider.svelte
@@ -5,12 +5,28 @@
 	import { createEventDispatcher, tick } from 'svelte';
 	import { initStores } from './stores';
 
+	// Wrapper class that prevents an exception in svelte-devtools
+	// It hides the CachedSyncIterable in a non-enumerable property
+	// causing it to be ignored when svelte-devtools clones the component state.
+	class CachedSyncIterableWrapper<T> {
+		// @ts-expect-error property gets assigned via Object.defineProperty
+		private iterable: CachedSyncIterable<T>;
+
+		constructor(iterable: CachedSyncIterable<T>) {
+			Object.defineProperty(this, 'iterable', { value: iterable, enumerable: false });
+		}
+
+		[Symbol.iterator]() {
+			return this.iterable[Symbol.iterator]();
+		}
+	}
+
 	export let bundles: Iterable<FluentBundle> = [];
 
 	const initialTick = tick();
 	const dispatch = createEventDispatcher<{ error: string }>();
 
-	$: cachedBundles = CachedSyncIterable.from(bundles);
+	$: cachedBundles = new CachedSyncIterableWrapper(CachedSyncIterable.from(bundles));
 
 	const bundlesStore = writable(cachedBundles);
 	$: $bundlesStore = cachedBundles;
