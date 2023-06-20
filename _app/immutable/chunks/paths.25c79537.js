5b7d2760933e7187adedcd90d032fb5d1bf89e58
sfriedel <sfriedel@users.noreply.github.com>
2023-06-20 08:06:39 +0000

    deploy: 5031645296d0d7108871659e838741f20f7d7239

---
+++ _app/immutable/chunks/paths.25c79537.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_5pqqj8)==null?void 0:s.base)??"/svelte-fluent";var e;const a=((e=globalThis.__sveltekit_5pqqj8)==null?void 0:e.assets)??t;export{a,t as b};
---
+++ _app/immutable/nodes/5.f300cd2d.js
@@ -0,0 +1,1 @@
+import{P as m}from"../chunks/_page.svelte.cb598aec.js";export{m as component};
