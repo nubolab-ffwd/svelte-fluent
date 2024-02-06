89dbfd50617ba86e91cc63dacb0569ca64dc3d75
Sebastian Friedel <sebastian@ffwd.consulting>
2024-02-06 12:20:58 +0100

    chore(deps): update sveltekit to v2

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
