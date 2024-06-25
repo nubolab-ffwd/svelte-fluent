379bee76770883ab1b1843879c56b3e7b786985d
Sebastian Friedel <sebastian@ffwd.consulting>
2024-06-25 12:22:04 +0200

    test: add e2e test for readme example

---
+++ src/e2e/readme-example/Test.svelte
@@ -0,0 +1,33 @@
+<script>
+	import { FluentBundle, FluentResource } from '@fluent/bundle';
+	import { Localized, initFluentContext, createSvelteFluent } from '@nubolab-ffwd/svelte-fluent';
+
+	export let userName = 'Anna';
+	export let userGender = 'female';
+	export let photoCount = 3;
+
+	const translations = `
+# Simple things are simple.
+hello-user = Hello, {$userName}!
+
+# Complex things are possible.
+shared-photos =
+    {$userName} {$photoCount ->
+        [one] added a new photo
+       *[other] added {$photoCount} new photos
+    } to {$userGender ->
+        [male] his stream
+        [female] her stream
+       *[other] their stream
+    }.
+`;
+	const bundle = new FluentBundle('en');
+	bundle.addResource(new FluentResource(translations));
+
+	initFluentContext(() => createSvelteFluent([bundle]));
+</script>
+
+<strong><Localized id="hello-user" args={{ userName }} /></strong>
+<p>
+	<Localized id="shared-photos" args={{ userName, userGender, photoCount }} />
+</p>
