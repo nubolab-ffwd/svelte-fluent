6527920456c3cecfd30b183ea7f38e477c8323df
Sebastian Friedel <sebastian@ffwd.consulting>
2024-10-14 16:07:03 +0200

    test: compatibility with @testing-library/svelte@5.2

--- src/tests/setup.js
+++ src/tests/setup.js
@@ -1,10 +1,4 @@
-import * as svelteInternal from 'svelte/internal';
-import { vi, beforeEach, afterEach, beforeAll } from 'vitest';
-
-beforeAll(() => {
-	// workaround for vitest issue https://github.com/vitest-dev/vitest/issues/2834
-	vi.mock('svelte', () => svelteInternal);
-});
+import { vi, beforeEach, afterEach } from 'vitest';
 
 beforeEach(() => {
 	vi.useFakeTimers();
