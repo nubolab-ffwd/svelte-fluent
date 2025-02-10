280fa132160cc8abc6b3c7cf0a4c883a148c0b75
Sebastian Friedel <sebastian@ffwd.consulting>
2025-02-10 13:08:27 +0100

    chore(examples): cleanup some type errors

--- examples/src/index.ts
+++ examples/src/index.ts
@@ -1,3 +1,4 @@
+/// <reference types="vite/client" />
 const exampleComponents = import.meta.glob('./**/App.svelte');
 
 export const allExamples = Object.entries(exampleComponents).map(([name, mod]) => ({
