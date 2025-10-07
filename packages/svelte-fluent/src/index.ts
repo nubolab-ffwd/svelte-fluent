30d3b9d1a2d8c11a5d45441a3ff335400dae7f10
Sebastian Friedel <sebastian@ffwd.consulting>
2025-10-07 15:56:50 +0200

    chore!: remove deprecated `FluentProvider` component

--- packages/svelte-fluent/src/index.ts
+++ packages/svelte-fluent/src/index.ts
@@ -7,5 +7,4 @@ export {
 } from './internal/context.svelte';
 export { ComponentElement, TagElement } from './internal/elements.js';
 export { createSvelteFluent, type SvelteFluent } from './internal/fluent.js';
-export { default as FluentProvider } from './internal/FluentProvider.svelte';
 export { default as Localized } from './internal/Localized.svelte';
